1bdf33ea349146c54ba1c1265d8ae6d340b2d22f
Vitali177 <vskrabatun@gmail.com>
2023-05-29 18:47:21 +0300

    converting errors to responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,12 +4,18 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof RequestValidationError) {
-    console.log('Handling this error as RequestValidationError');
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === 'field') {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
 
   if (err instanceof DatabaseConnectionError) {
     console.log('Handling this error as DatabaseConnectionError');
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
 
-  return res.status(400).send({ message: err.message });
+  return res.status(400).send({ errors: [{ message: 'Something went wrong' }] });
 };
