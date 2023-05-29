d059bac2b01bc90cb951138effb52f6a7568d1c8
Vitali177 <vskrabatun@gmail.com>
2023-05-29 09:07:45 +0300

    remove mock error message

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,5 +3,5 @@ import { Request, Response, NextFunction } from 'express';
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log('Something wend wrong', err);
 
-  return res.status(400).send({ message: 'Something wend wrong' });
+  return res.status(400).send({ message: err.message });
 };
