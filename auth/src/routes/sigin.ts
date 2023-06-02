844f1921409c6204a9f7dd278a7b4dbeb49db5cd
Vitali177 <vskrabatun@gmail.com>
2023-06-02 07:54:51 +0300

    add sign in logic

--- auth/src/routes/sigin.ts
+++ auth/src/routes/sigin.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
+import { Password } from '../services/password';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
@@ -18,8 +20,34 @@ router.post('/api/users/signin',
       .withMessage('You must supply a password')
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    return res.send('Hi there!');
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session
+    req.session = {
+      jwt: userJwt
+    };
+
+    return res.status(200).send(existingUser);
   }
 );
 
