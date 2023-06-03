73813e6917bd274b8268015a112d2946156b6e99
Vitali177 <vskrabatun@gmail.com>
2023-06-03 20:34:44 +0300

    add successful account sign up

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,13 +1,17 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    const res = await axios.post('/api/users/signup', {
+      email, password
+    });
+    console.log(res.data);
   };
 
   return (
