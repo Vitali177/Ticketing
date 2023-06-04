c57cd5e3fcc2ee0f18d76e2208dd56e5f639d4e4
Vitali177 <vskrabatun@gmail.com>
2023-06-04 20:10:47 +0300

    specifying the host for browser and server

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,8 +6,24 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser');
-  return response.data;
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx...
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
 };
 
 export default LandingPage;
