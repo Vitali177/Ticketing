03c94eb24c1c0d2ad3dd086e74ea44127b3a0e04
Vitali177 <vskrabatun@gmail.com>
2023-06-04 17:15:54 +0300

    create custom useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+import useRequest from '../../hooks/use-request';
+
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
