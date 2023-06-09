01b8b61d96b62df3f52ca007d2c269a42135e6f7
Vitali177 <vskrabatun@gmail.com>
2023-06-09 09:19:19 +0300

    ticket creation via Route handler

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,14 +1,24 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@vitali177_tickets/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
 router.post('/api/tickets', requireAuth, [
   body('title').not().isEmpty().withMessage('Title is required'),
   body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
-], validateRequest, (req: Request, res: Response) => {
-  return res.sendStatus(200);
+], validateRequest, async (req: Request, res: Response) => {
+  const { title, price } = req.body;
+
+  const ticket = Ticket.build({
+    title,
+    price,
+    userId: req.currentUser!.id
+  });
+  await ticket.save();
+
+  return res.status(201).send(ticket);
 });
 
 export { router as createTicketRouter };
