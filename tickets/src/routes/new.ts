a74a9120dd665821cd022ebe3709200d08f3e16d
Vitali177 <vskrabatun@gmail.com>
2023-06-16 14:31:07 +0300

    trying to call ticket created publisher

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@vitali177_tickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 
 const router = express.Router();
 
@@ -17,6 +18,10 @@ router.post('/api/tickets', requireAuth, [
     userId: req.currentUser!.id
   });
   await ticket.save();
+  new TicketCreatedPublisher(client).publish({
+    id: ticket.id,
+    ...ticket
+  });
 
   return res.status(201).send(ticket);
 });
