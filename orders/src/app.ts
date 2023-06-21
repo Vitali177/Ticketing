ac3142d9957ddd952bfd7e756d98a0d559e1a72b
Vitali177 <vskrabatun@gmail.com>
2023-06-21 20:44:27 +0300

    first test for Orders service, transfer routes folder

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,10 +4,10 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@vitali177_tickets/common';
 
-import { newOrderRouter } from '../routes/new';
-import { showOrderRouter } from '../routes/show';
-import { deleteOrderRouter } from '../routes/delete';
-import { indexOrderRouter } from '../routes';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
 import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@vitali177_tickets/common';
 import { body } from 'express-validator';
-import { Ticket } from '../src/models/ticket';
-import { Order } from '../src/models/order';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
