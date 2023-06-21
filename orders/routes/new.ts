1aaa1aeac0fd64c80a0aa32f0709f3bd360a8b17
Vitali177 <vskrabatun@gmail.com>
2023-06-21 12:54:29 +0300

    subtle service coupling

--- orders/routes/new.ts
+++ orders/routes/new.ts
@@ -1,9 +1,17 @@
+import mongoose, { mongo } from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@vitali177_tickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  return res.send({});
-});
+router.post('/api/orders', requireAuth, [
+  body('ticketId').not().isEmpty().custom((input: string) => mongoose.Types.ObjectId.isValid(input)).withMessage('Ticket id must be provided')
+],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    return res.send({});
+  }
+);
 
 export { router as newOrderRouter };
