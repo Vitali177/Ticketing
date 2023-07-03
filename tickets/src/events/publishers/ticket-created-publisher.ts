66ac94757977631f1dd6fb328ec4786b80f24bca
Vitali177 <vskrabatun@gmail.com>
2023-07-03 12:33:55 +0300

    fix compile error for tickets service

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,4 +1,16 @@
-import { Publisher, Subjects, TicketCreatedEvent } from "@vitali177_tickets/common";
+import { Publisher, Subjects } from "@vitali177_tickets/common";
+
+interface TicketCreatedEvent {
+  subject: Subjects.TicketCreated;
+  data: {
+    id: string;
+    version: number;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
+
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
