28d6f71614e8a892e7feec2020f45a093cccd7bc
Vitali177 <vskrabatun@gmail.com>
2023-07-03 13:37:16 +0300

    start testing listeners

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import { app } from "../../app";
+
+const setup = async () => {
+  // create the instance of a listener
+
+  //create a fake data event
+
+  // create a fake message object
+};
+
+it('creates and saves a ticket', async () => {
+  // call the onMessage function with the data object + message object
+
+  //write assertion to make sure a ticket was created! 
+});
+
+it('acks the message', async () => {
+  // call the onMessage function with the data object + message object
+
+  // write assertions to make sure ack function is called
+});
