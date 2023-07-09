e36eff68b2ed9d99a0c996354da200c3c78184ee
Vitali177 <vskrabatun@gmail.com>
2023-07-09 10:36:27 +0300

    mock function arguments

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { Message } from 'node-nats-streaming';
-import { OrderCreatedEvent, OrderStatus } from '@vitali177_tickets/common';
+import { OrderCreatedEvent, OrderStatus, Subjects } from '@vitali177_tickets/common';
 import { natsWrapper } from '../../../nats-wrapper';
 import { Ticket } from '../../../models/ticket';
 import { OrderCreatedListener } from '../order-created-listener';
@@ -52,4 +52,13 @@ it('acks the message', async () => {
   await listener.onMessage(data, msg);
 
   expect(msg.ack).toHaveBeenCalled();
+});
+
+it('publishes a ticket updated event', async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+  const ticketUpdatedEventName = (natsWrapper.client.publish as jest.Mock).mock.calls[0][0];
+  expect(ticketUpdatedEventName).toEqual(Subjects.TicketUpdated);
 });
