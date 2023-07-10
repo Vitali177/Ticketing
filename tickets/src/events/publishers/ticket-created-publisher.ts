4644a27c71dc0157a0c51a2450e04930c8fe39f4
Vitali177 <vskrabatun@gmail.com>
2023-07-10 19:35:30 +0300

    order cancelled listener

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,4 +1,15 @@
-import { Publisher, Subjects, TicketCreatedEvent } from "@vitali177_tickets/common";
+import { Publisher, Subjects } from "@vitali177_tickets/common";
+
+interface TicketCreatedEvent {
+  subject: Subjects.TicketCreated;
+  data: {
+    id: string;
+    version: number;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -1,4 +1,16 @@
-import { Publisher, Subjects, TicketUpdatedEvent } from "@vitali177_tickets/common";
+import { Publisher, Subjects } from "@vitali177_tickets/common";
+
+interface TicketUpdatedEvent {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    version: number;
+    title: string;
+    price: number;
+    userId: string;
+    orderId?: string;
+  };
+}
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
