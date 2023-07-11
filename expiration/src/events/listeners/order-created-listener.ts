6d4743b73f07ae94c4ef6930316fe9037701fad6
Vitali177 <vskrabatun@gmail.com>
2023-07-11 09:39:01 +0300

    listener creation

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,12 @@
+import { Listener, Subjects, OrderCreatedEvent } from '@vitali177_tickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    
+  }
+}
