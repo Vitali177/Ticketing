f693a87a0f796f0649e8e751d9bbd90b5e77c458
Vitali177 <vskrabatun@gmail.com>
2023-07-13 17:33:19 +0300

    testing same-user validation

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
@@ -33,11 +33,11 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   process.env.JWT_KEY = 'asdfasdf';
   // Build a JWT payload { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com'
   };
 
