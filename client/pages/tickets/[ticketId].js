5c501927bb8d7a344eac39dbe79362f9ce3c2cbb
Vitali177 <vskrabatun@gmail.com>
2023-07-23 15:09:37 +0300

    linking to wildcard routes

---
+++ client/pages/tickets/[ticketId].js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const TicketShow = () => {
+
+  return (
+    <div>Ticket Show</div>
+  );
+};
+
+export default TicketShow;
