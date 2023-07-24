f413674a028e1816f18c83c433a4f98f7e0331da
Vitali177 <vskrabatun@gmail.com>
2023-07-24 16:53:34 +0300

    add header links

--- client/components/header.js
+++ client/components/header.js
@@ -4,12 +4,14 @@ export default ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: 'auth/signup' },
     !currentUser && { label: 'Sign In', href: 'auth/signin' },
+    currentUser && { label: 'Sell tickets', href: '/tickets/new' },
+    currentUser && { label: 'My orders', href: '/orders' },
     currentUser && { label: 'Sign Out', href: 'auth/signout' }
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
       return <li key={href} className="nav-item">
-        <Link href={href}>
+        <Link href={href} className="nav-link">
           {label}
         </Link>
       </li>
